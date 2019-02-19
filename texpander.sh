5f7a5403f9322e1aee338cbd6cf51204fdd03900
Aurèle <eaurele@users.noreply.github.com>
2019-02-19 17:47:36 +0100

    Simplify a bit

--- texpander.sh
+++ texpander.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
+!/bin/bash
 
 # Version: 2.0
 # Release: November 24, 2017
-
-# Get window id, pass to getwindow pid to output the pid of current window
-pid=$(xdotool getwindowfocus getwindowpid)
-
-# Store text name of process based on pid of current window
-proc_name=$(cat /proc/$pid/comm)
-
-# If ~/.texpander directory does not exist, create it
-if [ ! -d ${HOME}/.texpander ]; then
-    mkdir ${HOME}/.texpander
-fi
 
 # Store base directory path, expand complete path using HOME environemtn variable
 base_dir=$(realpath "${HOME}/.texpander")
@@ -40,26 +29,6 @@ then
     # Put text in clipboard selection for apps like Firefox that 
     # insist on using the clipboard for all pasting
     echo -n "$(cat "$path")" | xsel -b -i
-
-    # Paste text into current active window
-    sleep 0.3
-    xdotool key shift+Insert
-
-    # If you're having trouble pasting into apps, use xdotool
-    # to type into the app instead. This is a little bit slower
-    # but may work better with some applications.
-    #
-    # Make xdotool type RETURN instead of LINEFEED characters 
-    # otherwise some apps like Gmail in Firefox won't recognize
-    # newline characters.
-    #
-    # To use this, comment out line #32 (xdotool key shift+Insert)
-    # and uncomment the line below.
-    #xdotool type -- "$(xsel -bo | tr \\n \\r | sed s/\\r*\$//)"
-
-    # Restore the original value of the clipboard
-    sleep 0.5
-    echo $clipboard | xsel -b -i
 
   else
     zenity --error --text="Abbreviation not found:\n${name}"
